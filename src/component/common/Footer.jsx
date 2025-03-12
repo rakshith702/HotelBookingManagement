a05cd345aa4b9df5c132010da6cdeff5c625a10f
M Rakshith <83411446+rakshith702@users.noreply.github.com>
2025-03-12 11:52:50 +0530

    Initial commit

---
+++ src/component/common/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="my-footer">
+      <span> Phegon Hotel | All Rights Reserved &copy; 2025</span>
+    </footer>
+  );
+};
+export default Footer;
